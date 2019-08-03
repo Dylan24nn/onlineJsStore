0248c308556b26586df1ed6751c4918e543db583
Denis Smirnovnn <appianway87@gmail.com>
2019-08-03 21:45:18 +0300

    1. Добавлены функции для работы с куки 2. Создан Json объект описывающий товары 3. Добавлена функция избранные товары

---
+++ functions.js
@@ -0,0 +1,48 @@
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+//function getPriceById(id) {
+//    return goods.items[id].price;
+//}
+//let itemId = 2;
+//let priceId = getPriceById(itemId);
+//console.log(priceId);
+
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    var username = getCookie("username");
+    if (username != "") {
+        alert("Welcome again " + username);
+    } else {
+        username = prompt("Please enter your name:", "");
+        if (username != "" && username != null) {
+            setCookie("username", username, 365);
+        }
+    }
+}
+setCookie("x", 4, 7);
+setCookie("y", 8, 7);
+setCookie("z", 21, 7);
+
+//console.log(parseInt(getCookie("x"))+ parseInt(getCookie("y"))+parseInt(getCookie("z")));
+
